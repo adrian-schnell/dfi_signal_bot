b91b46bfd4269e60c7312104eed663bb2fb235a8
Adrian <adrian@c-m-d.info>
2021-07-07 14:30:08 +0200

    Add notification when a masternode is enabled

---
+++ app/Listeners/MnEnabledListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Enum\QueueNames;
+use App\Events\MnEnabledEvent;
+use App\Jobs\MnEnabledNotificationJob;
+use App\Models\UserMasternode;
+
+class MnEnabledListener
+{
+    public function handle(MnEnabledEvent $event): void
+    {
+        $event->getMasternode()->userMasternodes->each(function (UserMasternode $userMasternode) {
+            ray($userMasternode);
+            dispatch(new MnEnabledNotificationJob($userMasternode))
+                ->onQueue(QueueNames::TELEGRAM_MESSAGE_OUTGOING);
+        });
+    }
+}
