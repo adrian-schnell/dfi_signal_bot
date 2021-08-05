f5da9c4743fa0e52dc2c5a12b180171241cce44b
Adrian <adrian@c-m-d.info>
2021-08-05 07:21:44 +0200

    bugfix local dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,13 +4,14 @@ namespace App\Providers;
 
 use BotMan\BotMan\BotMan;
 use Illuminate\Support\ServiceProvider;
+use Str;
 use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        if (!app()->environment('local')) {
+        if (!app()->environment('local') || Str::startsWith(config('app.url'), 'https')) {
             URL::forceScheme('https');
         }
         $this->app->singleton(BotMan::class, function ($app) {
