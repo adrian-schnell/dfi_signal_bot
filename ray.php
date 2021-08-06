1e16ca0006118368e39eff7dc115e0b083541c73
Adrian <adrian@c-m-d.info>
2021-08-06 19:28:47 +0200

    cleanup ray update .env.example

--- ray.php
+++ ray.php
@@ -67,13 +67,13 @@ return [
      * Absolute base path for your sites or projects in Homestead,
      * Vagrant, Docker, or another remote development server.
      */
-    'remote_path'           => env('RAY_REMOTE_PATH', '/var/www'),
+    'remote_path'           => env('RAY_REMOTE_PATH'),
 
     /*
      * Absolute base path for your sites or projects on your local
      * computer where your IDE or code editor is running on.
      */
-    'local_path'            => env('RAY_LOCAL_PATH', '/Users/adrian/Code/DFI-Signal'),
+    'local_path'            => env('RAY_LOCAL_PATH'),
 
     /*
      * When this setting is enabled, the package will not try to format values sent to Ray.
