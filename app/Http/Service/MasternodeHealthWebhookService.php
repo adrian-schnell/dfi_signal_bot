a54a13b751aa5b4e6ff01d9e1404b31fb925a6b8
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-10-01 18:38:48 +0200

    refactor the message string generation - remove the duplicated code

--- app/Http/Service/MasternodeHealthWebhookService.php
+++ app/Http/Service/MasternodeHealthWebhookService.php
@@ -9,6 +9,8 @@ use Str;
 
 class MasternodeHealthWebhookService
 {
+    const TYPE_WARNINGS = 'warnings';
+    const TYPE_CRITICAL_ERROR = 'critical';
     protected TelegramUser $telegramUser;
     protected array $data;
     protected array $analysis;
@@ -52,138 +54,17 @@ class MasternodeHealthWebhookService
 
     protected function sendWarnings(TelegramMessageService $messageService): void
     {
-        $message = '';
-        $blockHeight = $this->searchInArray($this->analysis['warnings'], 'block_height');
-        $cooldownKeyBlockHeight = $this->generateCooldownKey('warnings', 'block_height');
-        if (
-            count($blockHeight) > 0
-            && $this->telegramUser->cooldown($cooldownKeyBlockHeight)->passed()
-        ) {
-            $message = __('mn_health_webhook.warnings.block_height', [
-                'local_height'  => $blockHeight['value'],
-                'remote_height' => $blockHeight['expected'],
-                'difference'    => abs($blockHeight['value'] - $blockHeight['expected']),
-            ]);
-            $this->telegramUser->cooldown($cooldownKeyBlockHeight)->until(now()->addHours(2));
-        }
+        $message = $this->generateMessage(self::TYPE_WARNINGS, 'block_height', 2);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'connection_count', 1);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'logsize', 6);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'config_checksum', 12);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'node_version', 12);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'load_avg', 1);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'hdd', 24);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'ram', 24);
+        $message .= $this->generateMessage(self::TYPE_WARNINGS, 'server_script_version', 24);
 
-        $connectionCount = $this->searchInArray($this->analysis['warnings'], 'block_height');
-        $cooldownKeyConnectionCount = $this->generateCooldownKey('warnings', 'connection_count');
-        if (
-            count($connectionCount) > 0
-            && $this->telegramUser->cooldown($cooldownKeyConnectionCount)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.connection_count', [
-                    'count' => $connectionCount['value'],
-                ]);
-            $this->telegramUser->cooldown($cooldownKeyConnectionCount)->until(now()->addHours(1));
-        }
-
-        $logsize = $this->searchInArray($this->analysis['warnings'], 'logsize');
-        $cooldownKeyLogSize = $this->generateCooldownKey('warnings', 'logsize');
-        if (
-            count($logsize) > 0
-            && $this->telegramUser->cooldown($cooldownKeyLogSize)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.logsize', [
-                    'size' => $logsize['value'],
-                ]);
-            $this->telegramUser->cooldown($cooldownKeyLogSize)->until(now()->addHours(6));
-        }
-
-        $configChecksum = $this->searchInArray($this->analysis['warnings'], 'config_checksum');
-        $cooldownKeyConfigChecksum = $this->generateCooldownKey('warnings', 'config_checksum');
-        if (
-            count($configChecksum) > 0
-            && $this->telegramUser->cooldown($cooldownKeyConfigChecksum)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.config_checksum');
-            $this->telegramUser->cooldown($cooldownKeyConfigChecksum)->until(now()->addHours(12));
-        }
-
-        $nodeVersion = $this->searchInArray($this->analysis['warnings'], 'node_version');
-        $cooldownKeyNodeVersion = $this->generateCooldownKey('warnings', 'node_version');
-        if (
-            count($nodeVersion) > 0
-            && $this->telegramUser->cooldown($cooldownKeyNodeVersion)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.node_version', [
-                    'active_version' => $nodeVersion['value'],
-                    'new_version'    => $nodeVersion['expected'],
-                ]);
-            $this->telegramUser->cooldown($cooldownKeyNodeVersion)->until(now()->addHours(12));
-        }
-
-        $loadAvg = $this->searchInArray($this->analysis['warnings'], 'load_avg');
-        $cooldownKeyLoadAvg = $this->generateCooldownKey('warnings', 'load_avg');
-        if (
-            count($loadAvg) > 0
-            && $this->telegramUser->cooldown($cooldownKeyLoadAvg)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.load_avg', [
-                    'value' => $loadAvg['value'],
-                ]);
-            $this->telegramUser->cooldown($cooldownKeyLoadAvg)->until(now()->addHours(1));
-        }
-
-        $hdd = $this->searchInArray($this->analysis['warnings'], 'hdd');
-        $cooldownKeyHdd = $this->generateCooldownKey('warnings', 'hdd');
-        if (
-            count($hdd) > 0
-            && $this->telegramUser->cooldown($cooldownKeyHdd)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.hdd', [
-                    'percent' => $hdd['value'],
-                ]);
-            $this->telegramUser->cooldown($cooldownKeyHdd)->until(now()->addDay());
-        }
-
-        $ram = $this->searchInArray($this->analysis['warnings'], 'ram');
-        $cooldownKeyHdd = $this->generateCooldownKey('warnings', 'ram');
-        if (
-            count($ram) > 0
-            && $this->telegramUser->cooldown($cooldownKeyHdd)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.ram', [
-                    'percent' => $ram['value'],
-                ]);
-            $this->telegramUser->cooldown($cooldownKeyHdd)->until(now()->addDay());
-        }
-
-        $serverScript = $this->searchInArray($this->analysis['warnings'], 'server_script_version');
-        $cooldownKeyHdd = $this->generateCooldownKey('warnings', 'server_script_version');
-        if (
-            count($serverScript) > 0
-            && $this->telegramUser->cooldown($cooldownKeyHdd)->passed()
-        ) {
-            $message = "\r\n\r\n" . __('mn_health_webhook.warnings.server_script_version', [
-                    'installed_version' => $serverScript['value'],
-                    'new_version'       => $serverScript['expected'],
-                ]);
-            $this->telegramUser->cooldown($cooldownKeyHdd)->until(now()->addDay());
-        }
-
-        // finally send the message
-        if (Str::length($message) > 0) {
-            $messageService->sendMessage(
-                $this->telegramUser,
-                __('mn_health_webhook.headline.warnings'),
-                ['parse_mode' => 'Markdown']
-            );
-            $messageService->sendMessage(
-                $this->telegramUser,
-                $message,
-                ['parse_mode' => 'Markdown']
-            );
-            $messageService->sendMessage(
-                $this->telegramUser,
-                __('mn_health_webhook.latest_server_update', [
-                    'date'           => $this->latestUpdate->format('d.m.Y H:i:s'),
-                    'human_readable' => time_diff_humanreadable(now(), $this->latestUpdate, $this->telegramUser->language),
-                ]),
-                ['parse_mode' => 'Markdown']
-            );
-        }
+        $this->sendMessageWithCategory($messageService, self::TYPE_WARNINGS, $message);
     }
 
     protected function sendCriticalErrors(TelegramMessageService $messageService): void
@@ -205,5 +86,54 @@ class MasternodeHealthWebhookService
     protected function generateCooldownKey(string $category, string $type): string
     {
         return sprintf('%s_%s_%s', $category, $type, md5($this->telegramUser->id));
+    }
+
+    protected function generateMessage(string $category, string $type, int $cooldownHours): string
+    {
+        $array = $this->searchInArray($this->analysis[$category], $type);
+        $cooldownKey = $this->generateCooldownKey($category, $type);
+        if (
+            count($array) > 0
+            && $this->telegramUser->cooldown($cooldownKey)->passed()
+        ) {
+            $this->telegramUser->cooldown($cooldownKey)->until(now()->addHours($cooldownHours));
+
+            return "\r\n\r\n" . __(sprintf('mn_health_webhook.%s.%s', $category, $type), [
+                    'value'      => $array['value'],
+                    'expected'   => $array['expected'],
+                    'difference' => abs((int)($array['value'] ?? 0) - (int)($array['expected'] ?? 0)),
+                ]);
+        }
+
+        return '';
+    }
+
+    protected function sendMessageWithCategory(
+        TelegramMessageService $messageService,
+        string $category,
+        string $message
+    ): void {
+        // finally send the message
+        if (Str::length($message) > 0) {
+            $messageService->sendMessage(
+                $this->telegramUser,
+                __(sprintf('mn_health_webhook.headline.%s', $category)),
+                ['parse_mode' => 'Markdown']
+            );
+            $messageService->sendMessage(
+                $this->telegramUser,
+                $message,
+                ['parse_mode' => 'Markdown']
+            );
+            $messageService->sendMessage(
+                $this->telegramUser,
+                __('mn_health_webhook.latest_server_update', [
+                    'date'           => $this->latestUpdate->format('d.m.Y H:i:s'),
+                    'human_readable' => time_diff_humanreadable(now(), $this->latestUpdate,
+                        $this->telegramUser->language),
+                ]),
+                ['parse_mode' => 'Markdown']
+            );
+        }
     }
 }
--- resources/lang/en/mn_health_webhook.php
+++ resources/lang/en/mn_health_webhook.php
@@ -8,16 +8,16 @@ return [
     'latest_server_update' => 'Data based on latest server update :human_readable (:date)',
 
     'warnings' => [
-        'block_height'     => "⚠️ *Block Height*\r\nYour local node has block height :local_height and the main net is at block :remote_height. The node is :difference blocks behind.",
-        'connection_count' => "⚠️ *Connection Count*\r\nAt the moment there are only :count connections to the node. Maybe the node recently restartet. Otherwise it's recommended to add more connections.",
+        'block_height'     => "⚠️ *Block Height*\r\nYour local node has block height :value and the main net is at block :expected. The node is :difference blocks behind.",
+        'connection_count' => "⚠️ *Connection Count*\r\nAt the moment there are only :value connections to the node. Maybe the node recently restartet. Otherwise it's recommended to add more connections.",
         'logsize'          => "⚠️ *Logsize quite big*\r\nYour `debug.log` appears to be quite bit with :size MB. Maybe your node has a problem...",
         'config_checksum'  => "⚠️ *Config Checksum changed*\r\nThe checksum of your `defi.conf` file changed. If you did not change anything, take a look at this file. Maybe your server got hacked..?",
-        'node_version'     => "⚠️ *Please update your node*\r\nYour node is running on version :active_version - the current available version is :new_version. Please update your node soon.",
+        'node_version'     => "⚠️ *Please update your node*\r\nYour node is running on version :value - the current available version is :expected. Please update your node soon.",
 
         'load_avg'              => "⚠️ *Server load*\r\nThe current system load of your server (:value) is quite high and needs your attention!",
-        'hdd'                   => "⚠️ *HDD capacity*\r\nAt the moment, :percent percent of your HDD capacity is used.",
-        'ram'                   => "⚠️ *RAM capacity*\r\nAt the moment, :percent percent of your RAM capacity is used.",
-        'server_script_version' => "⚠️ *Server Script needs update*\r\nInstalled version is :installed_version, but current version is :new_version. Please upgrade with `pip3 install --upgrade masternode-health`",
+        'hdd'                   => "⚠️ *HDD capacity*\r\nAt the moment, :value percent of your HDD capacity is used.",
+        'ram'                   => "⚠️ *RAM capacity*\r\nAt the moment, :value percent of your RAM capacity is used.",
+        'server_script_version' => "⚠️ *Server Script needs update*\r\nInstalled version is :value, but current version is :expected. Please upgrade with `pip3 install --upgrade masternode-health`",
     ],
 
     'critical' => [
