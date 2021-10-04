a1a5ab0eb9f8b486f4af976de289cd03f7bbaa2f
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-10-04 18:06:34 +0200

    remove nova code

--- config/nova-links.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'links' => [
-        'Queues' => sprintf('%s/admin_backend/queues', config('app.url')),
-        'Debug'  => sprintf('%s/admin_backend/debug', config('app.url')),
-    ],
-];
