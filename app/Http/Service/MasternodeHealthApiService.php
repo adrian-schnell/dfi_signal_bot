28032274273abaed80eb44d214e7ec1ec374e61c
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-10 09:11:09 +0100

    increase timeout for loading server data add hint to wait

--- app/Http/Service/MasternodeHealthApiService.php
+++ app/Http/Service/MasternodeHealthApiService.php
@@ -11,150 +11,150 @@ use Throwable;
 
 class MasternodeHealthApiService
 {
-    protected ClientInterface $client;
-    protected TelegramUser $user;
+	protected ClientInterface $client;
+	protected TelegramUser $user;
 
-    public function __construct(TelegramUser $user)
-    {
-        $this->client = new Client([
-            'base_uri'        => config('masternode_health.base_uri'),
-            'timeout'         => 5,
-            'connect_timeout' => 5,
-        ]);
-        $this->user = $user;
-    }
+	public function __construct(TelegramUser $user)
+	{
+		$this->client = new Client([
+			'base_uri'        => config('masternode_health.base_uri'),
+			'timeout'         => 5,
+			'connect_timeout' => 5,
+		]);
+		$this->user = $user;
+	}
 
-    /**
-     * @throws MasternodeHealthApiException
-     */
-    public function setupApiKey(): self
-    {
-        try {
-            $response = json_decode(
-                $this->client->get(config('masternode_health.endpoints.setup_api_key'), [
-                    'timeout'            => 5,
-                    'connection_timeout' => 5,
-                ])->getBody()->getContents(),
-                true
-            );
-            $this->user->update([
-                'server_health_api_key' => $response['api_key'],
-            ]);
+	/**
+	 * @throws MasternodeHealthApiException
+	 */
+	public function setupApiKey(): self
+	{
+		try {
+			$response = json_decode(
+				$this->client->get(config('masternode_health.endpoints.setup_api_key'), [
+					'timeout'            => 5,
+					'connection_timeout' => 5,
+				])->getBody()->getContents(),
+				true
+			);
+			$this->user->update([
+				'server_health_api_key' => $response['api_key'],
+			]);
 
-            return $this;
-        } catch (Throwable $e) {
-            Log::error('failed creating api key', [
-                'file'    => $e->getFile(),
-                'message' => $e->getMessage(),
-                'line'    => $e->getLine(),
-            ]);
-            throw MasternodeHealthApiException::generic($e->getMessage(), $e);
-        }
-    }
+			return $this;
+		} catch (Throwable $e) {
+			Log::error('failed creating api key', [
+				'file'    => $e->getFile(),
+				'message' => $e->getMessage(),
+				'line'    => $e->getLine(),
+			]);
+			throw MasternodeHealthApiException::generic($e->getMessage(), $e);
+		}
+	}
 
-    /**
-     * @throws MasternodeHealthApiException
-     */
-    public function setupWebhook(): self
-    {
-        try {
-            $this->client->post(config('masternode_health.endpoints.webhook_create'), [
-                'timeout'            => 5,
-                'connection_timeout' => 5,
-                'headers'            => [
-                    'x-api-key' => $this->user->server_health_api_key,
-                ],
-                'form_params'        => [
-                    'url'       => route('webhook.masternode_health'),
-                    'reference' => encrypt($this->user->id),
-                ],
-            ]);
+	/**
+	 * @throws MasternodeHealthApiException
+	 */
+	public function setupWebhook(): self
+	{
+		try {
+			$this->client->post(config('masternode_health.endpoints.webhook_create'), [
+				'timeout'            => 5,
+				'connection_timeout' => 5,
+				'headers'            => [
+					'x-api-key' => $this->user->server_health_api_key,
+				],
+				'form_params'        => [
+					'url'       => route('webhook.masternode_health'),
+					'reference' => encrypt($this->user->id),
+				],
+			]);
 
-            return $this;
-        } catch (Throwable $e) {
-            Log::error('failed creating webhook', [
-                'file'    => $e->getFile(),
-                'message' => $e->getMessage(),
-                'line'    => $e->getLine(),
-            ]);
-            throw MasternodeHealthApiException::generic($e->getMessage(), $e);
-        }
-    }
+			return $this;
+		} catch (Throwable $e) {
+			Log::error('failed creating webhook', [
+				'file'    => $e->getFile(),
+				'message' => $e->getMessage(),
+				'line'    => $e->getLine(),
+			]);
+			throw MasternodeHealthApiException::generic($e->getMessage(), $e);
+		}
+	}
 
-    /**
-     * @throws MasternodeHealthApiException
-     */
-    public function deleteWebhook(): self
-    {
-        try {
-            $this->client->delete(config('masternode_health.endpoints.webhook_delete'), [
-                'timeout'            => 5,
-                'connection_timeout' => 5,
-                'headers'            => [
-                    'x-api-key' => $this->user->server_health_api_key,
-                ],
-            ]);
+	/**
+	 * @throws MasternodeHealthApiException
+	 */
+	public function deleteWebhook(): self
+	{
+		try {
+			$this->client->delete(config('masternode_health.endpoints.webhook_delete'), [
+				'timeout'            => 5,
+				'connection_timeout' => 5,
+				'headers'            => [
+					'x-api-key' => $this->user->server_health_api_key,
+				],
+			]);
 
-            return $this;
-        } catch (Throwable $e) {
-            Log::error('failed deleting webhook', [
-                'file'    => $e->getFile(),
-                'message' => $e->getMessage(),
-                'line'    => $e->getLine(),
-            ]);
-            throw MasternodeHealthApiException::generic($e->getMessage(), $e);
-        }
-    }
+			return $this;
+		} catch (Throwable $e) {
+			Log::error('failed deleting webhook', [
+				'file'    => $e->getFile(),
+				'message' => $e->getMessage(),
+				'line'    => $e->getLine(),
+			]);
+			throw MasternodeHealthApiException::generic($e->getMessage(), $e);
+		}
+	}
 
-    /**
-     * @throws MasternodeHealthApiException
-     */
-    public function getNodeInfo(): array
-    {
-        try {
-            return json_decode(
-                $this->client->get(config('masternode_health.endpoints.pull_node_info'), [
-                    'timeout'            => 5,
-                    'connection_timeout' => 5,
-                    'headers'            => [
-                        'x-api-key' => $this->user->server_health_api_key,
-                    ],
-                ])->getBody()->getContents(),
-                true
-            );
-        } catch (Throwable $e) {
-            Log::error('failed loading node info', [
-                'file'    => $e->getFile(),
-                'message' => $e->getMessage(),
-                'line'    => $e->getLine(),
-            ]);
-            throw MasternodeHealthApiException::generic($e->getMessage(), $e);
-        }
-    }
+	/**
+	 * @throws MasternodeHealthApiException
+	 */
+	public function getNodeInfo(): array
+	{
+		try {
+			return json_decode(
+				$this->client->get(config('masternode_health.endpoints.pull_node_info'), [
+					'timeout'            => 30,
+					'connection_timeout' => 5,
+					'headers'            => [
+						'x-api-key' => $this->user->server_health_api_key,
+					],
+				])->getBody()->getContents(),
+				true
+			);
+		} catch (Throwable $e) {
+			Log::error('failed loading node info', [
+				'file'    => $e->getFile(),
+				'message' => $e->getMessage(),
+				'line'    => $e->getLine(),
+			]);
+			throw MasternodeHealthApiException::generic($e->getMessage(), $e);
+		}
+	}
 
-    /**
-     * @throws MasternodeHealthApiException
-     */
-    public function getServerStats(): array
-    {
-        try {
-            return json_decode(
-                $this->client->get(config('masternode_health.endpoints.pull_server_stats'), [
-                    'timeout'            => 5,
-                    'connection_timeout' => 5,
-                    'headers'            => [
-                        'x-api-key' => $this->user->server_health_api_key,
-                    ],
-                ])->getBody()->getContents(),
-                true
-            );
-        } catch (Throwable $e) {
-            Log::error('failed loading server stats', [
-                'file'    => $e->getFile(),
-                'message' => $e->getMessage(),
-                'line'    => $e->getLine(),
-            ]);
-            throw MasternodeHealthApiException::generic($e->getMessage(), $e);
-        }
-    }
+	/**
+	 * @throws MasternodeHealthApiException
+	 */
+	public function getServerStats(): array
+	{
+		try {
+			return json_decode(
+				$this->client->get(config('masternode_health.endpoints.pull_server_stats'), [
+					'timeout'            => 30,
+					'connection_timeout' => 5,
+					'headers'            => [
+						'x-api-key' => $this->user->server_health_api_key,
+					],
+				])->getBody()->getContents(),
+				true
+			);
+		} catch (Throwable $e) {
+			Log::error('failed loading server stats', [
+				'file'    => $e->getFile(),
+				'message' => $e->getMessage(),
+				'line'    => $e->getLine(),
+			]);
+			throw MasternodeHealthApiException::generic($e->getMessage(), $e);
+		}
+	}
 }
--- resources/lang/de/serverHealthConversation.php
+++ resources/lang/de/serverHealthConversation.php
@@ -1,21 +1,22 @@
 <?php
 
 return [
-    'no_data' => "❌ Es wurden noch keine Serverdaten übertragen. Hast du die Einrichtungsschritte alle korrekt befolgt und mindestens einen Cron-Aufruf gewartet?\r\n\r\nAnsonsten führe diesen Befehl von Hand auf deinem Server aus:\r\n`masternode-health --verbose --report --api-key=your-api-key`",
-    'latest_update' => "\r\n\r\nzuletzt aktualisiert: :time",
+	'loading_data'  => 'Kurz Geduld... das Laden der Server Infos kann bis zu einer Minute dauern.',
+	'no_data'       => "❌ Es wurden noch keine Serverdaten übertragen. Hast du die Einrichtungsschritte alle korrekt befolgt und mindestens einen Cron-Aufruf gewartet?\r\n\r\nAnsonsten führe diesen Befehl von Hand auf deinem Server aus:\r\n`masternode-health --verbose --report --api-key=your-api-key`",
+	'latest_update' => "\r\n\r\nzuletzt aktualisiert: :time",
 
-    'server_stats' => [
-        'disk_usage'  => "Festplatte:\t:used/:total GB\r\n `:progress`",
-        'ram_usage'   => "\r\n\r\nArbeitsspeicher:\t:used/:total GB\r\n `:progress`",
-        'system_load' => "\r\n\r\nSystemlast:\r\n `:progress`",
-    ],
-    'nodeInfo'     => [
-        'block_height'     => "*Block Höhe:*\t\t\t:value",
-        'block_hash'       => "\r\n*Block Hash:*\t\t\t\t[:value](:link)",
-        'log_size'         => "\r\n*Log Datei Größe:*\t\t:value MB",
-        'connection_count' => "\r\n*Verbindungen:* :value",
-        'node_version'     => "\r\n*Node Version:* :value",
-        'node_uptime'      => "\r\n*Node Betriebszeit:* :value",
-        'operator_status'  => "\r\n\r\n*Operator Status:*",
-    ],
+	'server_stats' => [
+		'disk_usage'  => "Festplatte:\t:used/:total GB\r\n `:progress`",
+		'ram_usage'   => "\r\n\r\nArbeitsspeicher:\t:used/:total GB\r\n `:progress`",
+		'system_load' => "\r\n\r\nSystemlast:\r\n `:progress`",
+	],
+	'nodeInfo'     => [
+		'block_height'     => "*Block Höhe:*\t\t\t:value",
+		'block_hash'       => "\r\n*Block Hash:*\t\t\t\t[:value](:link)",
+		'log_size'         => "\r\n*Log Datei Größe:*\t\t:value MB",
+		'connection_count' => "\r\n*Verbindungen:* :value",
+		'node_version'     => "\r\n*Node Version:* :value",
+		'node_uptime'      => "\r\n*Node Betriebszeit:* :value",
+		'operator_status'  => "\r\n\r\n*Operator Status:*",
+	],
 ];
