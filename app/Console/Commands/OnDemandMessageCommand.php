71fabac1c49c926fcbdb130ce87f93b5ce95f5e6
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-23 15:02:51 +0100

    add debug mode

--- app/Console/Commands/OnDemandMessageCommand.php
+++ app/Console/Commands/OnDemandMessageCommand.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 
 class OnDemandMessageCommand extends Command
 {
-	protected $signature = 'signal:on-demand {--message=} {--markdown} {--language=}';
+	protected $signature = 'signal:on-demand {--message=} {--markdown} {--language=} {--debug}';
 	protected $description = 'Send a custom on demand message to all users';
 
 	public function handle(TelegramMessageService $messageService)
@@ -16,6 +16,7 @@ class OnDemandMessageCommand extends Command
 		$message = $this->option('message');
 		$parseMarkdownOption = $this->option('markdown');
 		$language = $this->option('language');
+		$debug = $this->option('debug');
 		if (\Str::length($message) < 5) {
 			$this->error('please enter a message');
 
@@ -33,12 +34,14 @@ class OnDemandMessageCommand extends Command
 		$this->line('start sending out messages...');
 		$this->line('########################################');
 		$this->withProgressBar($users, function (TelegramUser $user)
-		use ($messageService, $message, $parseMarkdownOption) {
+		use ($messageService, $message, $parseMarkdownOption, $debug) {
 			$parseMarkdown = [];
 			if ($parseMarkdownOption) {
 				$parseMarkdown = ['parse_mode' => 'Markdown'];
 			}
-			$messageService->sendMessage($user, $message, $parseMarkdown);
+			if (!$debug) {
+				$messageService->sendMessage($user, $message, $parseMarkdown);
+			}
 		});
 	}
 }
