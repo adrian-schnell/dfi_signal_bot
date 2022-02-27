224d4122031729dcac7bcb8b1522846a4eb17589
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-02-27 10:37:54 +0100

    add remaining freeze time in human readable string for /stats command

--- app/Models/Repository/IndexMemoryRepository.php
+++ app/Models/Repository/IndexMemoryRepository.php
@@ -2,36 +2,35 @@
 
 namespace App\Models\Repository;
 
-use app\Http\Service\DefichainApiService;
 use App\Models\IndexMemory;
 use App\Models\MintedBlock;
-use App\Models\TelegramUser;
-use App\Models\UserMasternode;
-use App\SignalService\SignalService;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Collection;
 
 class IndexMemoryRepository
 {
-    const LATEST_BLOCK_HEIGHT = 'latest_block_height';
+	const LATEST_BLOCK_HEIGHT = 'latest_block_height';
 
-    public function getLatestBlockIndex(): IndexMemory
-    {
-        try {
-            $index = IndexMemory::where('type', self::LATEST_BLOCK_HEIGHT)->firstOrFail();
-        } catch (ModelNotFoundException $e) {
-            $index = IndexMemory::create([
-                'type'  => self::LATEST_BLOCK_HEIGHT,
-                'value' => MintedBlock::orderByDesc('mintBlockHeight')->first()->mintBlockHeight ?? 0,
-            ]);
-        }
+	public function getLatestBlockIndex(): IndexMemory
+	{
+		try {
+			$index = IndexMemory::where('type', self::LATEST_BLOCK_HEIGHT)->firstOrFail();
+		} catch (ModelNotFoundException $e) {
+			$index = IndexMemory::create([
+				'type'  => self::LATEST_BLOCK_HEIGHT,
+				'value' => MintedBlock::orderByDesc('mintBlockHeight')->first()->mintBlockHeight ?? 0,
+			]);
+		}
 
-        return $index;
-    }
+		return $index;
+	}
 
-    public function incrementBlockHeightIndex(): void
-    {
-        $this->getLatestBlockIndex()->increment('value');
-    }
+	public function blockDifferenceToBlock(int $block): int
+	{
+		return $block - $this->getLatestBlockIndex()->value;
+	}
+
+	public function incrementBlockHeightIndex(): void
+	{
+		$this->getLatestBlockIndex()->increment('value');
+	}
 }
