9e87baf95cc8af6514e29473e772d69382c258fc
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-03-06 16:47:29 +0100

    command can use single user or all with option selection

--- app/Console/Commands/MintedBlockSignal.php
+++ app/Console/Commands/MintedBlockSignal.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 
 class MintedBlockSignal extends Command
 {
-	protected $signature = 'signal:update-masternode-minted {user}';
+	protected $signature = 'signal:update-masternode-minted {user?}';
 	protected $description = 'Load minted block for a user manually';
 
 	public function handle()
@@ -16,9 +16,32 @@ class MintedBlockSignal extends Command
 		$user = TelegramUser::find($this->argument('user'));
 
 		if (isset($user)) {
-			$this->info(sprintf('updating minted blocks for user %s %s (ID: %s)', $user->firstName, $user->lastName,
-				$user->id));
-			app(DefichainApiService::class)->storeMintedBlockForTelegramUser($user);
+			$this->updateMintedBlocks($user);
+
+			return;
 		}
+
+		$choice = $this->choice('Do you want to update the blocks for all users?', [
+			'Yes',
+			'no',
+		]);
+
+		if ($choice !== 'Yes') {
+			$this->info('cancel command now...');
+
+			return;
+		}
+
+		$users = TelegramUser::all();
+		$users->each(function (TelegramUser $user) {
+			$this->updateMintedBlocks($user);
+		});
+	}
+
+	protected function updateMintedBlocks(TelegramUser $user): void
+	{
+		$this->info(sprintf('updating minted blocks for user %s %s (ID: %s)', $user->firstName, $user->lastName,
+			$user->id));
+		app(DefichainApiService::class)->storeMintedBlockForTelegramUser($user);
 	}
 }
